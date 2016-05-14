bf7f16c915de246b80a9cd5c438ff654d2d74943
Christian Tietze <christian.tietze@gmail.com>
2016-05-14 12:27:47 +0200

    add interactive mode, expand README

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
 # Usage
 
-Download the script and execute it like so:
+## Interactive
+
+Execute this command in your Terminal and follow the instructions:
+
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Weltenbastler/install/master/install_wiki.rb)"
+
+This will create an empty project and push dummy contents to your repository and its `gh-pages` branch.
+
+## Download first
+
+[Download the script](https://raw.githubusercontent.com/Weltenbastler/install/master/install_wiki.rb) and execute it like so:
 
     ruby install_wiki.rb <Git remote address> <path>
   
 * **Git remote address**: an empty GitHub repository where you are going to host the wiki. Typically of the form `git@github.com:USERNAME/REPOSITORY.git`.
 * **path**: the path in your file system where you want to store the files. Can be `~/Documents/PROJECTNAME` or similar.
 
+The result will be the same :)
 
+# What then?
+
+The script will tell you where to go from there. When the empty nanoc wiki is initialized, you can `cd` into the project directory and edit text files in the `content/` subfolder. 
+
+* You can compile a **local preview** using `nanoc compile`. The result will be in the project's `output/` folder.
+* You can also show the page as if it was online using `nanoc view`. This will start a simple webserver on your machine. Head to <http://localhost:3000/> once it started up successfully.
+* While the server runs, you can run `nanoc compile` to update its contents.
+   
+  **Note:** this command will run in your Terminal until you exit with Ctrl-C, so you cannot type anything yourself while it runs. Start it with a trailing `&` to move it into background mode: `nanoc view &`. Alternatively, use another shell session for this process (recommended).
+ 
+When you're satisfied, commit your changes locally:
+
+    git add .
+    git commit -m "<describe your changes here>"
+    
+You can upload them with `git push origin master` if you want.
+
+To publish the website, run `rake publish`.
--- install_wiki.rb
+++ install_wiki.rb
@@ -3,15 +3,16 @@
 require 'optparse'
 require 'fileutils'
 require 'open3'
+require 'readline'
 
 TEMPLATE_URL = "https://github.com/Weltenbastler/template-mac.git"
 
 # Print banner when arguments are missing
-ARGV << '-h' if ARGV.count < 2
+ARGV << '-h' if ARGV.count > 0 && ARGV.count < 2
 
 options = {}
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: #{__FILE__}.rb [options] <Git remote address> <path>"
+  opts.banner = "Usage: #{__FILE__}.rb [options] [<Git remote address> <path>]"
 
   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
     options[:verbose] = v
@@ -48,8 +49,6 @@ private def execute(command, options, error_msg = "Aborting")
 end
 
 def initialize_from_template(options)
-  puts "Cloning from template ..."
-  
   commands = [
     %Q(git clone #{TEMPLATE_URL} . #{"--progress" if options[:verbose]}),
     %Q(rm -rf .git),
@@ -82,18 +81,53 @@ def build_initial_site(options)
   execute("bundle exec rake publish", options, "Building the initial site failed")
 end
 
+def ask_for_input(question)
+  Readline.readline("#{question} ").strip
+end
+
+def guard_repo_exists(repo, options)
+  puts "Checking repository ..." if options[:verbose]
+  
+  success = Open3.popen3("git ls-remote #{repo}") do |stdin, stdout, stderr, wait_thr|
+    wait_thr.value.success?
+  end
+  
+  abort("Repository address invalid") unless success
+  puts "Repository exist!" if options[:verbose] 
+end
 
 # 
 # The actual command sequence 
 #
 
-path = ARGV.pop
-options[:path] = File.expand_path(path, __dir__)
-options[:address] = ARGV.pop
+if ARGV.count > 0
+  path = ARGV.pop
+  options[:path] = File.expand_path(path, __dir__)
+  
+  options[:address] = ARGV.pop
+else
+  path = ask_for_input("Where do you want to create the project?")
+  abort("Path required") if path.empty?
+  options[:path] = path
+  
+  repo = ask_for_input("What is your remote Git repository address?")
+  guard_repo_exists(repo, options)
+  options[:address] = repo
+end
 
 FileUtils.mkdir_p(options[:path])
 guard_empty_dir(options)
+puts "Cloning from template ..."
 initialize_from_template(options)
+puts ""
+puts "Setting up project ..."
 configure_bundle(options)
 configure_git(options)
+puts ""
+puts "Generating website output ..."
 build_initial_site(options)
+puts ""
+puts "Finished!"
+puts ""
+puts "You can now go to your project folder and edit away:"
+puts "  $ cd #{options[:path]}/content"
